81e87de239ea42bb464ba944e7fce2f917b81cad
Alimerdan Rahimov <94564048+AliDevel@users.noreply.github.com>
2024-10-09 18:35:53 +0200

    Update rocket.go

--- rocket/rocket.go
+++ rocket/rocket.go
@@ -145,7 +145,7 @@ log.WithField("message", "afterRun").Debug("hello")
 }
 
 func (rock *RocketCon) run() {
-	return 0
+	return nil
 	log.WithField("message", "Method").Debug("run")
 	// Set some websocket tunables
 	const socketreadsizelimit = 65536
